980db37d72086849d6359dc6bb463dab730f1ee2
Evgenii Kuznetsov <harsel1234@gmail.com>
2018-03-10 20:38:50 +0400

    Add catalog page template

--- src/components/pages/Catalog.vue
+++ src/components/pages/Catalog.vue
@@ -1,9 +1,24 @@
 <template>
   <b-container>
-    <p>Каталог</p>
-    <p>
-      <b-link to="/object-card/1">Карточка объекта</b-link>
-    </p>
+    <h1 class="display-3 mt-3 mb-4">Каталог</h1>
+    <b-row>
+      <template v-for="rowIndex in 3">
+        <b-col v-for="colIndex in 3" :key="`catalog-${rowIndex}${colIndex}`" class="mb-4">
+          <b-card title="Card Title"
+                  img-src="https://lorempixel.com/600/300/food/5/"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  class="box-shadow">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+        </b-col>
+        <div class="w-100"></div>
+      </template>
+    </b-row>
   </b-container>
 </template>
 
